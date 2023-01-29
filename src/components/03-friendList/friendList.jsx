9e15a4e43e013784179ef78fa690cdc9361b74a2
Mateusz Martin <mateusz.martinn@gmail.com>
2023-01-29 22:37:12 +0100

    stats\style & friendList\style

--- src/components/03-friendList/friendList.jsx
+++ src/components/03-friendList/friendList.jsx
@@ -1,19 +1,26 @@
+import friendsCSS from './friends.module.css';
+
 const FriendList = friendListProps => {
   return (
-    <ul>
+    <ul className={friendsCSS.friendList}>
       {friendListProps.friends.map(friend => (
-        <li className="item" key={friend.id}>
+        <li className={friendsCSS.friend} key={friend.id}>
           <span
-            className="status"
+            className={friendsCSS.status}
             is-online={friend.isOnline.toString()}
+            style={
+              friend.isOnline
+                ? { backgroundColor: '#22f938' }
+                : { backgroundColor: '#f33535' }
+            }
           ></span>
           <img
-            className="avatar"
+            className={friendsCSS.avatar}
             src={friend.avatar}
             alt="User avatar"
             width="48"
           />
-          <p className="name">{friend.name}</p>
+          <p className={friendsCSS.name}>{friend.name}</p>
         </li>
       ))}
     </ul>

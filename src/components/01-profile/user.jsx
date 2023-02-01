286df28860b2ebf1a61365ad0d242f526f955cc1
Mateusz Martin <mateusz.martinn@gmail.com>
2023-02-01 01:29:22 +0100

    PropTypes and destructuring assignment has been implemented

--- src/components/01-profile/user.jsx
+++ src/components/01-profile/user.jsx
@@ -1,40 +1,44 @@
+import PropTypes from 'prop-types';
+
 import userCSS from './user.module.css';
 import { usersIcon, eyeIcon, likeIcon } from 'components/svgs';
-const Profile = profileProps => {
+const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div className={userCSS.profile}>
       <div className={userCSS.description}>
-        <img
-          src={profileProps.avatar}
-          alt="User avatar"
-          className={userCSS.avatar}
-        />
-        <p className={userCSS.name}>{profileProps.username}</p>
-        <p className={userCSS.tag}>@{profileProps.tag}</p>
-        <p className={userCSS.location}>{profileProps.location}</p>
+        <img src={avatar} alt="User avatar" className={userCSS.avatar} />
+        <p className={userCSS.name}>{username}</p>
+        <p className={userCSS.tag}>@{tag}</p>
+        <p className={userCSS.location}>{location}</p>
       </div>
 
       <ul className={userCSS.stats}>
         <li className={userCSS.statsItem}>
           <span className={userCSS.label}>Followers</span>
-          <span className={userCSS.quantity}>
-            {profileProps.stats.followers}
-          </span>
+          <span className={userCSS.quantity}>{stats.followers}</span>
         </li>
         {usersIcon}
 
         <li className={userCSS.statsItem}>
           {eyeIcon}
           <span className={userCSS.label}>Views</span>
-          <span className={userCSS.quantity}>{profileProps.stats.views}</span>
+          <span className={userCSS.quantity}>{stats.views}</span>
         </li>
         <li className={userCSS.statsItem}>
           {likeIcon}
           <span className={userCSS.label}>Likes</span>
-          <span className={userCSS.quantity}>{profileProps.stats.likes}</span>
+          <span className={userCSS.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
 };
 export default Profile;
